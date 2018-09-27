e6b594128ebe2bad7eee1f7c5fc756d297018917
sunilkandpal-git <sunilkandpal-git@users.noreply.github.com>
2018-09-27 12:41:29 +0530

    Added Enemy Spawn system

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
  [RequireComponent (typeof (CharacterController))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : LivingEntity {
      //handling
 	public float rotationSpeed = 450;
 	public float walkSpeed = 5;
@@ -17,7 +17,8 @@ public class PlayerController : MonoBehaviour {
 
 	//components
 	private CharacterController controller;
-	void Start () {
+	protected override void Start () {
+		base.Start();
 		controller = GetComponent<CharacterController>();
 		cam = Camera.main;
 	}
@@ -41,16 +42,16 @@ public class PlayerController : MonoBehaviour {
 		Vector3 mousePos =Input.mousePosition;
 		mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x , mousePos.y,cam.transform.position.y-transform.position.y));
 		targetRotation = Quaternion.LookRotation(mousePos- new Vector3(transform.position.x,0,transform.position.z));
-		 transform.eulerAngles = Vector3.up  * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+		transform.eulerAngles = Vector3.up  * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
 
 		Vector3 input =new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
-			currentVelocityMod =Vector3.MoveTowards(currentVelocityMod,input,acceleration * Time.deltaTime);
-			Vector3 motion = currentVelocityMod;
-			motion *=(Mathf.Abs(input.x)==1 && Mathf.Abs(input.z)==1)?.7f:1;
-			motion *=(Input.GetButton("Run"))?runSpeed:walkSpeed;
-			motion +=Vector3.up * -8;
+		currentVelocityMod =Vector3.MoveTowards(currentVelocityMod,input,acceleration * Time.deltaTime);
+		Vector3 motion = currentVelocityMod;
+		motion *=(Mathf.Abs(input.x)==1 && Mathf.Abs(input.z)==1)?.7f:1;
+		motion *=(Input.GetButton("Run"))?runSpeed:walkSpeed;
+		motion +=Vector3.up * -8;
 
-			controller.Move(motion * Time.deltaTime);
+		controller.Move(motion * Time.deltaTime);
 	}
 	void ControlWASD(){
 		Vector3 input =new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public Wave[] waves;
+	public Enemy enemy;
+
+	Wave currentWave;
+	int currentWaveCount;
+	int enemiesRemainingToSpawn;
+	int enemiesRamainingAlive;
+	float nextSpawnTime;
+
+	[System.Serializable]
+	public class Wave{
+		public int enemyCount;
+		public int timeBetweenSpawns;
+	}
+
+	// Use this for initialization
+	void Start () {
+		NextWave();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(enemiesRemainingToSpawn > 0 &&  Time.time > nextSpawnTime){
+			enemiesRemainingToSpawn--;
+			nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+			Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+			spawnedEnemy.OnDeath += OnEnemyDeath;
+		}
+	}
+
+	void OnEnemyDeath(){
+		enemiesRamainingAlive--;
+		if(enemiesRamainingAlive == 0){
+			NextWave();
+		}
+	}
+
+	void NextWave(){
+		currentWaveCount++;
+		if(currentWaveCount - 1 < waves.Length){
+			currentWave = waves[currentWaveCount -1];
+			enemiesRemainingToSpawn = currentWave.enemyCount;
+			enemiesRamainingAlive = enemiesRemainingToSpawn;
+		}
+	}
+}
